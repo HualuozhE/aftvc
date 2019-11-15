3d54fbb5a7c10a3b52a7d0894507e77e45136713
HualuozhE <hualuozhe@gmail.com>
2019-11-15 13:17:27 +0800

    2019-11-15 13:17:25

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,10 @@ const routes = [
           import(/* webpackChunkName: "homeOverviews" */ '../pages/OverView/OverView.vue')
       }
     ]
+  },
+  {
+    path: '/index.html',
+    redirect: '/'
   }
   // {
   //   path: '/about',
